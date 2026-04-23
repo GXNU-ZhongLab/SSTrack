d757376606141967fed79b2a00ece05d7eac4b02
Azong-HQU <1371119403@qq.com>
2026-04-23 10:35:43 +0800

    Update __init__.py

--- lib/train/dataset/__init__.py
+++ lib/train/dataset/__init__.py
@@ -11,21 +11,21 @@ from .coco_seq_lmdb import MSCOCOSeq_lmdb
 from .tracking_net_lmdb import TrackingNet_lmdb
 
 # Segmentation
-from .davis import Davis
-from .youtubevos import YouTubeVOS
-from .got10kvos import Got10kVOS
-from .lasotvos import LasotVOS
+# from .davis import Davis
+# from .youtubevos import YouTubeVOS
+# from .got10kvos import Got10kVOS
+# from .lasotvos import LasotVOS
 
-# Visual-Language
-from .ref_youtubevos import Refer_YouTubeVOS
-from .tnl2k import TNL2k
-from .tnl2k import TNL2k_Lang
-from .lasot_lang import Lasot_Lang
-from .otb_lang import OTB_Lang
-from .refcoco_seq import RefCOCOSeq
+# # Visual-Language
+# from .ref_youtubevos import Refer_YouTubeVOS
+# from .tnl2k import TNL2k
+# from .tnl2k import TNL2k_Lang
+# from .lasot_lang import Lasot_Lang
+# from .otb_lang import OTB_Lang
+# from .refcoco_seq import RefCOCOSeq
 
-# multi-modal tracking
-from .depthtrack import DepthTrack
-from .lasher import LasHeR
-from .visevent import VisEvent
-from .arkit import ARKit
+# # multi-modal tracking
+# from .depthtrack import DepthTrack
+# from .lasher import LasHeR
+# from .visevent import VisEvent
+# from .arkit import ARKit
